89f35597edacf5170c023e76dcb79be647f35fdb
Airton Tonello <airtonluizjunior@gmail.com>
2019-07-08 16:06:26 -0300

    comite

---
+++ MongoMvc/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using MongoMvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MongoMvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            PaisDB db = new PaisDB();
+
+            Pais p = new Pais();
+            p.Title = "Titulo";
+            p.Body = "Texto";
+            p.Created = DateTime.Now;
+            p.Active = true;
+            db.CriarPais(p);
+
+            db.EditarPais();
+
+            var list = db.PesquisaPais("o");
+
+            db.DeletarPais();
+
+            return View();
+        }
+    }
+}
---
+++ MongoMvc/Models/PaisDB.cs
@@ -0,0 +1,71 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Configuration;
+using System.Linq.Expressions;
+using MongoDB.Bson;
+
+namespace MongoMvc.Models
+{
+    public class PaisDB
+    {
+        IMongoClient client;
+        IMongoDatabase database;
+
+        public PaisDB()
+        {
+            client = new MongoClient(ConfigurationManager.ConnectionStrings["conexaoMongoDB"].ConnectionString);
+            database = client.GetDatabase("TestMongo");
+        }
+
+        public IMongoCollection<Pais> GetPais()
+        {
+            var pais = database.GetCollection<Pais>("pais");
+
+            return pais;
+        }
+
+        public void CriarPais(Pais pais)
+        {
+            IMongoCollection<Pais> db = database.GetCollection<Pais>("pais");
+            db.InsertOne(pais);
+        }
+
+        public void EditarPais()
+        {
+            var db = database.GetCollection<Pais>("pais");
+
+            Expression<Func<Pais, bool>> filter = x => x.Id.Equals(ObjectId.Parse("5d238c6032b6544830313bd6"));
+
+            Pais news = db.Find(filter).FirstOrDefault();
+
+            if (news != null)
+            {
+                news.Value = 200d;
+                ReplaceOneResult result = db.ReplaceOne(filter, news);
+            }
+        }
+
+        public IList<Pais> PesquisaPais(string titulo)
+        {
+            var db = database.GetCollection<Pais>("pais");
+
+            Expression<Func<Pais, bool>> filter = x => x.Title.Contains(titulo);
+
+            IList<Pais> items = db.Find(filter).ToList();
+
+            return items;
+        }
+
+        public void DeletarPais()
+        {
+            var db = database.GetCollection<Pais>("pais");
+
+            Expression<Func<Pais, bool>> filter = x => x.Id.Equals(ObjectId.Parse("5d238e2f32b65443d8ee416b"));
+
+            DeleteResult delresult = db.DeleteOne(filter);
+        }
+    }
+}
